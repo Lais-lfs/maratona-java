5b169c811d8a41f2df87d2e5a86cfc9b605ae116
Lais_lfs <laislara86@gmail.com>
2026-05-18 22:14:00 -0300

    Aula sobre Sobrescrita

---
+++ src/academy/devdojo/maratonajava/javacore/Isobrescrita/test/AnimeTest01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Isobrescrita.test;
+
+import academy.devdojo.maratonajava.javacore.Isobrescrita.metodo.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Fullmetal Alchemist");
+        System.out.println(anime);
+    }
+}
